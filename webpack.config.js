7053e090e071c61be88303e5a179eac455b81651
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-05 23:16:08 -0500

    build: set up module.exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['./src/index.html'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      title: 'TOD: Project Restaurant',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+    ],
+  },
+};
