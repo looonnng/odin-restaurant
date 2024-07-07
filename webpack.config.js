49ea4e5988bfb0b6d3090708fa84d17f2c481690
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-06 21:03:27 -0500

    build: add style and image loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
         test: /\.txt$/,
         use: 'raw-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
