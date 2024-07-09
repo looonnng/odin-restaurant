de6287944fc44785f5341660131e10a123a6f957
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-09 15:21:04 -0500

    refactor: adjust append method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { homePageLoad } from './homePageLoad.js';
+import { menuPageLoad } from './menuPageLoad.js';
+import { contactPageLoad } from './contactPageLoad.js';
 import './style.css';
 
-document.querySelector('#content').append(...homePageLoad());
+document.querySelector('#content').appendChild(homePageLoad());
