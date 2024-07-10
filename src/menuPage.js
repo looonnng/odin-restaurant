6a2043500b577d726642d2ee0ee0bb33ed40fdae
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-09 21:52:32 -0500

    feat: create utility functions

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,3 +1,5 @@
+import myImage from './images/placeholder1.png';
+
 // Utility Function
 const createElement = (type, classNames = [], text = '', attributes = {}) => {
   const element = document.createElement(type);
@@ -13,5 +15,28 @@ const createElement = (type, classNames = [], text = '', attributes = {}) => {
   return element;
 };
 
+const createSubtitle = (subtitle) => {
+  return createElement('h2', ['content__subtitle'], subtitle);
+};
+
+const createCardContainer = () => {
+  return createElement('div', ['card-container']);
+};
+
+const createCardWrapper = () => {
+  return createElement('div', ['card', 'wrapper', 'col']);
+};
+
+const createCardImage = (imageSource, altText) => {
+  const imageWrapper = createElement('div', ['card__image']);
+
+  const imageElement = new Image();
+  imageElement.src = imageSource;
+  imageElement.alt = altText;
+
+  imageWrapper.appendChild(imageElement);
+
+  return imageWrapper;
+};
 
 const loadMenuPage = () => {};
