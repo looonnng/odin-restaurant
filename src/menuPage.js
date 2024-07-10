a80abbb6dfc9f2f8989813890cef3b1f2f80ce3e
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-09 20:58:34 -0500

    feat: create utility function

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,3 +1,17 @@
-export const menuPageLoad = () => {
-  
-}
+// Utility Function
+const createElement = (type, classNames = [], text = '', attributes = {}) => {
+  const element = document.createElement(type);
+
+  element.classList.add(...classNames);
+
+  if (text) element.textContent = text;
+
+  Object.keys(attributes).forEach((attr) =>
+    element.setAttribute(attr, attributes[attr]),
+  );
+
+  return element;
+};
+
+
+const loadMenuPage = () => {};
