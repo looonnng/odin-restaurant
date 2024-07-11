06bae179236caf4454cff83de7b2f8ebf96795d5
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-10 22:06:59 -0500

    feat: make nav buttons functional

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import { loadMenuPage } from './menuPage.js';
 import { loadContactPage } from './contactPage.js';
 import './style.css';
 
-document.querySelector('#content').appendChild(loadHomePage());
+const handleNavBtn = (event) => {
+  const content = document.querySelector('#content');
+
+  if (event.target.textContent === 'Home')
+    content.replaceChildren(loadHomePage());
+  else if (event.target.textContent === 'Menu')
+    content.replaceChildren(loadMenuPage());
+  else content.replaceChildren(loadContactPage());
+};
+
+document.querySelectorAll('.nav__btn').forEach((button) => {
+  button.addEventListener('click', handleNavBtn);
+});
